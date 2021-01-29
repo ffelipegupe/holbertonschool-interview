53bada230304d262ceba20654e76d4362d6cef89
Felipe Gutierrez <1560@holbertonschool.com>
2021-01-29 13:10:40 -0500

    Add: new line

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -4,8 +4,16 @@ from sys import stdin
 
 
 codes_dic = {
-            "200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0,
-            "405": 0, "500": 0}
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
+
 size = 0
 
 
@@ -21,18 +29,14 @@ if __name__ == "__main__":
     count = 0
     try:
         for line in stdin:
-            try:
-                data = line.split()
-                size += int(data[-1])
-                if data[-2] in codes_dic:
-                    codes_dic[data[-2]] += 1
-            except Exception:
-                pass
-            if count == 9:
-                printing()
-                count = -1
             count += 1
+            data = line.split()
+            size += int(data[-1])
+            if data[-2] in codes_dic:
+                codes_dic[data[-2]] += 1
+            if count % 10 == 0:
+                printing()
     except KeyboardInterrupt:
+        pass
+    finally:
         printing()
-        raise
-    printing()
