cbdf3040668fa08e0d328ab45be148d2cae44381
Felipe Gutierrez <1560@holbertonschool.com>
2021-03-12 11:06:02 -0500

    Fix: editing lines

--- 0x0A-slide_line/slide_line.h
+++ 0x0A-slide_line/slide_line.h
@@ -1,11 +1,11 @@
 #ifndef SLIDE_LINE_H
 #define SLIDE_LINE_H
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-#define SLIDE_LEFT 1
-#define SLIDE_RIGHT 0
+#define SLIDE_LEFT 0
+#define SLIDE_RIGHT 1
 
 int slide_line(int *line, size_t size, int direction);
 
