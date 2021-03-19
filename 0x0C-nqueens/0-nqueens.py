bc492be97033cfc12b016b89544af0127643d982
Felipe Gutierrez <1560@holbertonschool.com>
2021-03-18 23:06:23 -0500

    Edit: editing function

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,43 +1,45 @@
 #!/usr/bin/python3
-'''
-Module that solves the N Queens puzzle
-'''
-from sys import argv, exit
+"""N queens"""
+import sys
+
+
+def approve_placement(board, row, col):
+    """Determine if it's safe for the queen to assume position"""
+    for a in range(col):
+        if board[a] is row or abs(board[a] - row) is abs(a - col):
+            return False
+    return True
+
+
+def Print_Board(board):
+    """Prints the solution and board"""
+    pos = []
+    board_size = len(board)
+    for row in range(board_size):
+        sub_array = [row, board[row]]
+        pos.append(sub_array)
+    print(pos)
+
+
+def Place_Queen(board, col):
+    """Places the queens safely on the board"""
+    if col is len(board):
+        Print_Board(board)
+        return
+    for row in range(len(board)):
+        if approve_placement(board, row, col):
+            board[col] = row
+            Place_Queen(board, col + 1)
+
 
 if __name__ == "__main__":
-    if len(argv) != 2:
-        print('Usage: nqueens N')
-        exit(1)
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N") or exit(1)
     try:
-        n = int(argv[1])
-    except BaseException:
-        print('N must be a number')
-        exit(1)
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number") or exit(1)
     if n < 4:
-        print('N must be at least 4')
-        exit(1)
-
-    solution = []
-
-    def solve_queens(row, n, solution):
-        if (row == n):
-            print(solution)
-        else:
-            for col in range(n):
-                placement = [row, col]
-                if valid_placement(solution, placement):
-                    solution.append(placement)
-                    solve_queens(row + 1, n, solution)
-                    solution.remove(placement)
-
-    def valid_placement(solution, placement):
-        for queen in solution:
-            if queen[1] == placement[1]:
-                return False
-            if (queen[0] + queen[1]) == (placement[0] + placement[1]):
-                return False
-            if (queen[0] - queen[1]) == (placement[0] - placement[1]):
-                return False
-        return True
-
-    solve_queens(0, n, solution)
+        print("N must be at least 4") or exit(1)
+    board = [[0 for col in range(n)] for row in range(n)]
+    Place_Queen(board, 0)
