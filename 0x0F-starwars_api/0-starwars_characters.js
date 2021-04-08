dec486041dafea3c16f524479b233c8f0588efd6
Felipe Gutierrez <1560@holbertonschool.com>
2021-04-07 23:05:42 -0500

    Edit: semistandard

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -3,14 +3,22 @@ const MovieId = process.argv[2];
 
 const request = require('request');
 
-request('https://swapi-api.hbtn.io/api/films/' + MovieId + '/', function (err, res, body) {
+const url = 'https://swapi-api.hbtn.io/api/films/' + MovieId + '/';
+
+request(url, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  }
   const json = JSON.parse(body);
   const values = json.characters;
   for (let i = 0; i < values.length; i++) {
     request(values[i], function (err, res, body) {
-	    const chars = JSON.parse(body);
-	    const name = chars.name;
+      if (err) {
+        console.log(err);
+      }
+      const chars = JSON.parse(body);
+      const name = chars.name;
       console.log(name);
     });
-	       }
+  }
 });
