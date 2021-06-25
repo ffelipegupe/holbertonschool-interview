83da02881185ebc281c8fff94c478ff715e5d330
Felipe Gutierrez <1560@holbertonschool.com>
2021-06-24 21:56:57 -0500

    Add: adding files

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Coins module """
+
+
+def makeChange(coins, total):
+    """ Function to determine the fewest number of coins """
+    if total == 0:
+        return 0
+    coins.sort()
+    res = 0
+    for i in range(0, len(coins)):
+        if coins[i] <= total:
+            total -= coins[i]
+            res += 1
+        elif coins[i] > total:
+            coins.pop()
+            
+            print(total)
+    return res
+            
+
+
+#print(makeChange([1, 2, 25], 37))
+print(makeChange([1, 2, 25], 37))
